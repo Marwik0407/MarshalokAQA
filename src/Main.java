5bfc4c3a661171e908d0473d67798478feb0261c
Marwik0407 <Evgenii_marshalok@mail.ru>
2025-03-10 18:49:50 +0300

    практическое задание 11(1)

---
+++ src/Main.java
@@ -0,0 +1,137 @@
+interface Shape {
+    double calculateArea();
+
+    String getFillColor();
+
+    String getBorderColor();
+
+    default double calculatePerimeter() {
+        return 0;
+    }
+}
+
+
+class Circle implements Shape {
+    private double radius;
+    private String fillColor;
+    private String borderColor;
+
+    public Circle(double radius, String fillColor, String borderColor) {
+        this.radius = radius;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+    private String fillColor;
+    private String borderColor;
+
+    public Rectangle(double width, double height, String fillColor, String borderColor) {
+        this.width = width;
+        this.height = height;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+
+class Triangle implements Shape {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private String fillColor;
+    private String borderColor;
+
+    public Triangle(double sideA, double sideB, double sideC, String fillColor, String borderColor) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double calculateArea() {
+        double s = calculatePerimeter() / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5, "Red", "Black");
+        Shape rectangle = new Rectangle(4, 6, "Blue", "Green");
+        Shape triangle = new Triangle(3, 4, 5, "Yellow", "Purple");
+
+        printShapeDetails(circle);
+        printShapeDetails(rectangle);
+        printShapeDetails(triangle);
+    }
+
+    private static void printShapeDetails(Shape shape) {
+        System.out.println("Периметр: " + shape.calculatePerimeter());
+        System.out.println("Площадь: " + shape.calculateArea());
+        System.out.println("Цвет заливки: " + shape.getFillColor());
+        System.out.println("Цвет границы: " + shape.getBorderColor());
+        System.out.println("------------------------------");
+    }
+}
